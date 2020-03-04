edcc92d7bdfa67313c4a5324425433be33bf0f52
Lee Hazlehurst <lee.hazlehurst@cantarus.com>
2020-03-04 15:50:08 +0000

    Module 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,9 @@ class QuizQuestion extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      incorrectAnswer: false
+    };
   }
 
   render() {
@@ -26,6 +28,10 @@ class QuizQuestion extends Component {
               )
             )}
           </ul>
+
+          {this.state.incorrectAnswer ? (
+            <p className="error">Sorry, that's not right</p>
+          ) : null}
         </section>
       </main>
     );
@@ -33,7 +39,10 @@ class QuizQuestion extends Component {
 
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false });
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnswer: true });
     }
   }
 }
